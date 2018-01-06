3e32d22d93ee1d0617717f0f6d3b93de935d260f
wjsc <walterscarano@outlook.com>
2018-01-05 21:11:32 -0300

    dev working super slim with node:alpine image

--- dev/api/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /app/
-npm install
-pm2 --name api start -x src/index.js --no-daemon --watch "./src"
--- dev/front/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /app/
-npm install
-npm run build
-pm2 --no-daemon start npm --name front -- run start

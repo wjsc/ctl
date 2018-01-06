b4bf830e07725689eec36d4012bb44b594207391
wjsc <walterscarano@outlook.com>
2018-01-05 22:08:45 -0300

    Working!

--- prd/front/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /app/
-npm install
-npm run build
-# pm2 --no-daemon start npm --name front -- run start
-serve -p 3000 -s build
--- prd/static/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /app/
-npm install
-pm2 --name static start -x src/index.js --no-daemon --watch "./src"
